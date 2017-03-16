d1cc970952fab0559ee529898a8c72c4976bbb4f
Ramun McCallum <matthew@synapseware.com>
2017-03-15 20:06:57 -0700

    Initial pass of converting to AVRA

--- src/pwmchndrvr.asm
+++ src/pwmchndrvr.asm
@@ -18,59 +18,59 @@
 ;		LED_DDRX_VALUES		(8bit value table, PGRM, element count = TOTAL_CHANNELS)
 ; ================================================================================================
 PwmChnDrvr_Main:
-	rcall	PwmChnDrvr_Shutdown
+		rcall	PwmChnDrvr_Shutdown
 
-	; Check the active LED channel against the total channels
-	; and only process PWM data if we've done a full refresh
-	cpi		rACTIVE_LED, TOTAL_CHANNELS
-	brlo	_pcd_activate_channel
-	clr		rACTIVE_LED
+		; Check the active LED channel against the total channels
+		; and only process PWM data if we've done a full refresh
+		cpi		rACTIVE_LED, TOTAL_CHANNELS
+		brlo	_pcd_activate_channel
+		clr		rACTIVE_LED
 
-	; Since we completed a full LED refresh,
-	; decay or reload the PWM values table
-	inc		rPWM_POS
-	cpi		rPWM_POS, 0							; if 0, then reload PWM table
-	breq	_pcd_seed							; rPWM_POS = 0, reload
-	rcall	PwmChnDrvr_decay					; rPWM_POS != 0, so decay first
-	rjmp	_pcd_activate_channel
-_pcd_seed:
-	rcall	PwmChnDrvr_Seed						; rPWM_POS is zero, so reload the data
+		; Since we completed a full LED refresh,
+		; decay or reload the PWM values table
+		inc		rPWM_POS
+		cpi		rPWM_POS, 0							; if 0, then reload PWM table
+		breq	_pcd_seed							; rPWM_POS = 0, reload
+		rcall	PwmChnDrvr_decay					; rPWM_POS != 0, so decay first
+		rjmp	_pcd_activate_channel
+	_pcd_seed:
+		rcall	PwmChnDrvr_Seed						; rPWM_POS is zero, so reload the data
 
-_pcd_activate_channel:
-	; First, turn on the active LED
-	; If the SRAM segment @ active_led index > 0, then turn on that LED
-	ldi		XH, HIGH(on_values)
-	ldi		XL, LOW(on_values)
-	clr		rtmp
-	add		XL, rACTIVE_LED
-	adc		XH, rtmp
-	ld		rtmp, X								; load current on_value
-	tst		rtmp
-	breq	_pcd_activate_next
-	ldi		ZH, HIGH(2*LED_PORT_VALUES)
-	ldi		ZL, LOW(2*LED_PORT_VALUES)			; load PORT pointer
-	clr		rtmp
-	add		ZL, rACTIVE_LED						; add active LED offset
-	adc		ZH, rtmp
-	lpm		rtmp, Z								; load port value
-	in		rmp, PORTB
-	or		rtmp, rmp
-	out		PORTB, rtmp
-	ldi		ZH, HIGH(2*LED_DDRX_VALUES)
-	ldi		ZL, LOW(2*LED_DDRX_VALUES)			; load DDRX pointer
-	clr		rtmp
-	add		ZL, rACTIVE_LED						; add active LED offset
-	adc		ZH, rtmp
-	lpm		rtmp, Z								; load ddr value
-	in		rmp, DDRB
-	or		rtmp, rmp
-	out		DDRB, rtmp
+	_pcd_activate_channel:
+		; First, turn on the active LED
+		; If the SRAM segment @ active_led index > 0, then turn on that LED
+		ldi		XH, HIGH(on_values)
+		ldi		XL, LOW(on_values)
+		clr		rtmp
+		add		XL, rACTIVE_LED
+		adc		XH, rtmp
+		ld		rtmp, X								; load current on_value
+		tst		rtmp
+		breq	_pcd_activate_next
+		ldi		ZH, HIGH(2*LED_PORT_VALUES)
+		ldi		ZL, LOW(2*LED_PORT_VALUES)			; load PORT pointer
+		clr		rtmp
+		add		ZL, rACTIVE_LED						; add active LED offset
+		adc		ZH, rtmp
+		lpm		rtmp, Z								; load port value
+		in		rmp, PORTB
+		or		rtmp, rmp
+		out		PORTB, rtmp
+		ldi		ZH, HIGH(2*LED_DDRX_VALUES)
+		ldi		ZL, LOW(2*LED_DDRX_VALUES)			; load DDRX pointer
+		clr		rtmp
+		add		ZL, rACTIVE_LED						; add active LED offset
+		adc		ZH, rtmp
+		lpm		rtmp, Z								; load ddr value
+		in		rmp, DDRB
+		or		rtmp, rmp
+		out		DDRB, rtmp
 
-_pcd_activate_next:
-	; Move to the next active LED.
-	; If we are done with all active LEDs, change our PWM counter	
-	inc		rACTIVE_LED
-	ret
+	_pcd_activate_next:
+		; Move to the next active LED.
+		; If we are done with all active LEDs, change our PWM counter	
+		inc		rACTIVE_LED
+		ret
 
 
 ; ================================================================================================
@@ -78,13 +78,13 @@ _pcd_activate_next:
 ;	Shut down all LED output
 ; ================================================================================================
 PwmChnDrvr_Shutdown:
-	in		rmp, DDRB
-	andi	rmp, LED_PORT_MASK
-	out		DDRB, rmp
-	in		rmp, PORTB
-	andi	rmp, LED_PORT_MASK
-	out		PORTB, rmp
-	ret
+		in		rmp, DDRB
+		andi	rmp, LED_PORT_MASK
+		out		DDRB, rmp
+		in		rmp, PORTB
+		andi	rmp, LED_PORT_MASK
+		out		PORTB, rmp
+		ret
 
 
 ; ================================================================================================
@@ -93,16 +93,16 @@ PwmChnDrvr_Shutdown:
 ;		rmp, rtmp, X
 ; ================================================================================================
 PwmChnDrvr_Clear:
-	ldi		XH, HIGH(on_values)
-	ldi		XL, LOW(on_values)
-	clr		rmp
-	clr		rtmp
-_pcd_clr_loop:
-	st		X+, rtmp
-	inc		rmp
-	cpi		rmp, TOTAL_CHANNELS
-	brlo	_pcd_clr_loop
-	ret
+		ldi		XH, HIGH(on_values)
+		ldi		XL, LOW(on_values)
+		clr		rmp
+		clr		rtmp
+	_pcd_clr_loop:
+		st		X+, rtmp
+		inc		rmp
+		cpi		rmp, TOTAL_CHANNELS
+		brlo	_pcd_clr_loop
+		ret
 
 
 ; ================================================================================================
@@ -117,40 +117,40 @@ _pcd_clr_loop:
 ;		BRIGHTNESS_DATA
 ; ================================================================================================
 PwmChnDrvr_Seed:
-	ldi		XH, HIGH(on_values)
-	ldi		XL, LOW(on_values)
-	clr		rmp
-_pcd_seed_loop:
-	ldi		ZH, HIGH(2*TABLE_MULT_OFFSET)		; get our offset...
-	ldi		ZL, LOW(2*TABLE_MULT_OFFSET)
-	add		ZL, rmp								; add loop index to the offset table address
-	brcc	PC+2
-	inc		ZH									; add 1 to MSB register if overflow from previous addition
-	ldi		YH, 0								; load table offset into Y register
-	lpm		YL, Z
-	add		YL, rOFFSET							; add our offset
-	brcc	PC+2
-	inc		YH
-	ldi		ZH, 0								; load our 16 bit compare value
-	ldi		ZL, LIGHT_TABLE_LEN
-	cp		YL, ZL								; compare 16 bit registers
-	cpc		YH, ZH
-	brlo	PC+4
-	sub		YL, ZL								; subtract if over limit...
-	sbc		YH, ZH
-_pcd_seed_1:
-	; Copy brightness values from table to SRAM segment
-	ldi		ZH, HIGH(2*BRIGHTNESS_DATA)
-	ldi		ZL, LOW(2*BRIGHTNESS_DATA)			; Z is pointer to program memory
-	add		ZL, YL								; add offset from rtmp to Z
-	brcc	PC+2								; skip over the next instruction (careful with the offset!)
-	inc		ZH
-	lpm		rtmp, Z								; load the brightness table value
-	st		X+, rtmp							; store the brightness table value
-	inc		rmp									; increment loop value
-	cpi		rmp, TOTAL_CHANNELS
-	brlo	_pcd_seed_loop						; continue loop if not done
-	ret
+		ldi		XH, HIGH(on_values)
+		ldi		XL, LOW(on_values)
+		clr		rmp
+	_pcd_seed_loop:
+		ldi		ZH, HIGH(2*TABLE_MULT_OFFSET)		; get our offset...
+		ldi		ZL, LOW(2*TABLE_MULT_OFFSET)
+		add		ZL, rmp								; add loop index to the offset table address
+		brcc	PC+2
+		inc		ZH									; add 1 to MSB register if overflow from previous addition
+		ldi		YH, 0								; load table offset into Y register
+		lpm		YL, Z
+		add		YL, rOFFSET							; add our offset
+		brcc	PC+2
+		inc		YH
+		ldi		ZH, 0								; load our 16 bit compare value
+		ldi		ZL, LIGHT_TABLE_LEN
+		cp		YL, ZL								; compare 16 bit registers
+		cpc		YH, ZH
+		brlo	PC+4
+		sub		YL, ZL								; subtract if over limit...
+		sbc		YH, ZH
+	_pcd_seed_1:
+		; Copy brightness values from table to SRAM segment
+		ldi		ZH, HIGH(2*BRIGHTNESS_DATA)
+		ldi		ZL, LOW(2*BRIGHTNESS_DATA)			; Z is pointer to program memory
+		add		ZL, YL								; add offset from rtmp to Z
+		brcc	PC+2								; skip over the next instruction (careful with the offset!)
+		inc		ZH
+		lpm		rtmp, Z								; load the brightness table value
+		st		X+, rtmp							; store the brightness table value
+		inc		rmp									; increment loop value
+		cpi		rmp, TOTAL_CHANNELS
+		brlo	_pcd_seed_loop						; continue loop if not done
+		ret
 
 
 ; ================================================================================================
@@ -159,16 +159,16 @@ _pcd_seed_1:
 ;		rmp, rtmp, X
 ; ================================================================================================
 PwmChnDrvr_decay:
-	ldi		XH, HIGH(on_values)
-	ldi		XL, LOW(on_values)
-	clr		rmp
-_pcd_decay_loop:
-	ld		rtmp, X
-	cpi		rtmp, 0
-	breq	PC+2
-	dec		rtmp
-	st		X+, rtmp
-	inc		rmp									; increment loop value
-	cpi		rmp, TOTAL_CHANNELS
-	brlo	_pcd_decay_loop						; continue loop if not done
-	ret
+		ldi		XH, HIGH(on_values)
+		ldi		XL, LOW(on_values)
+		clr		rmp
+	_pcd_decay_loop:
+		ld		rtmp, X
+		cpi		rtmp, 0
+		breq	PC+2
+		dec		rtmp
+		st		X+, rtmp
+		inc		rmp									; increment loop value
+		cpi		rmp, TOTAL_CHANNELS
+		brlo	_pcd_decay_loop						; continue loop if not done
+		ret
